d89698e541694775a492b6fdca4c506b35da1faf
Jonny B <jonnyb@Jonnys-MacBook-Pro.local>
2016-01-23 11:53:54 -0700

    first commit

---
+++ DontEatPete/PeteBtn.swift
@@ -0,0 +1,21 @@
+//
+//  PeteBtn.swift
+//  DontEatPete
+//
+//  Created by Jonny B on 1/14/16.
+//  Copyright © 2016 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class PeteBtn: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
